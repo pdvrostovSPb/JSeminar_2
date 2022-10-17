4c4ca9ef6ac95c1eb0b7c24c30dc23997f7e2720
aksdar31415@gmail.com <Zx38745!!!!!>
2022-10-17 19:38:49 +0300

    Initial commit

---
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -0,0 +1,18 @@
+package ru.gb.jseminar;
+
+public class Task2 {
+
+    //Напишите программу, которая принимает с консоли число в формате byte и записывает его в файл ”result.txt”.
+    //Требуется перехватить все возможные ошибки и вывести их в логгер.
+    //
+    //После написания, попробуйте подать на вход числа 100 и 200 и проследите разницу в результате
+    public static void main(String[] args) {
+
+    }
+
+    public String findJewelsInStones(String jewels, String stones){
+        
+        return "";
+    }
+
+}
---
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -0,0 +1,34 @@
+package ru.gb.jseminar;
+
+public class Task3 {
+
+    // Напишите программу, которая
+    // 1) по указанной строке URL скачивает файл и возвращает строковое содержимое файла
+    // 2) изменяет строковое содержимое файла подставляя по маске "%s" ваше имя
+    // 3) сохраняет файл локально
+    // 4) читает сохраненный файл и выводит содержимое в логгер
+    // 5) удаляет сохраненный файл
+    public static void main(String[] args) {
+        String fileUrl = "";
+
+    }
+
+    public static String download(String url, String localFilename) {
+
+        return "";
+    }
+
+    public static String change(String name, String fileContent) {
+
+        return "";
+    }
+
+    public static void saveOnLocal(String fileName, String fileContent) {
+
+    }
+
+    public boolean removeFromLocale(String fileName) {
+
+        return false;
+    }
+}
