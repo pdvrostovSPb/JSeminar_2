e0c0d6d155d861849cf515c0847148edd729904c
aksdar31415@gmail.com <Zx38745!!!!!>
2022-10-17 19:52:59 +0300

    Feature: add Homework.java

---
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -0,0 +1,23 @@
+package ru.gb.jseminar;
+
+public class Homework {
+
+    // Дана строка sql-запроса "select * from students".
+    // Сформируйте часть WHERE этого запроса, используя StringBuilder.
+    //
+    // 1) Данные для фильтрации подаются в метод двумя массивами paramName и paramValue.
+    // 2) Данные для фильтрации подаются в метод строкой в формате json.
+    public static void main(String[] args) {
+
+    }
+
+    public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
+
+        return "";
+    }
+
+    public String updateQueryByJson(String q, String[] paramName, String[] paramValue){
+
+        return "";
+    }
+}
